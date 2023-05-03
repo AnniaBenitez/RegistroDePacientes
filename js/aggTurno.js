39e570d009e843b48eead26f4768bc1920a700d1
Annia Benítez <anniamicaela@gmail.com>
2023-05-03 14:04:47 -0400

    Ya funciona la sección de doctores con el uso de API casera :D

--- js/aggTurno.js
+++ js/aggTurno.js
@@ -1,5 +1,5 @@
 const ACCESS_TOKEN =
-  "ya29.a0AWY7CkmsnvUPfayj0lUL83vBtTiY-RJxm52lpecj24EauYKDDVwxTQWgn9J5MbLEfxXTy23n7_-NH_Um0dq8VCHrvNUe2awC4wzwuVWVLruH_MC7jIiqfg0S_nQ1XePVF4PGQWC_35x37quQvNWPiWL_19u7aCgYKAR4SARESFQG1tDrp0TzW6QG5VRAxkQLrGdtdRw0163";
+  "https://sheets.googleapis.com/v4/spreadsheets/1Se6P1VkG8IlgK2N6TzphvumLt13yNMOBjEITNfKToko/values/enEspera";
  
 const SHEET_ID = '1Se6P1VkG8IlgK2N6TzphvumLt13yNMOBjEITNfKToko';
 
@@ -23,7 +23,7 @@ function onRegistrarDato(){
     data.values = values;
 
     fetch(
-        `https://sheets.googleapis.com/v4/spreadsheets/1Se6P1VkG8IlgK2N6TzphvumLt13yNMOBjEITNfKToko/values/enEspera!A1:Z1000`,
+        `https://sheets.googleapis.com/v4/spreadsheets/1Se6P1VkG8IlgK2N6TzphvumLt13yNMOBjEITNfKToko/values/enEspera`,
         {
         method: 'POST',
         headers: {
---
+++ js/scriptDoctores.js
@@ -0,0 +1,59 @@
+const ACCESS_TOKEN =
+  "ya29.a0AWY7Ckm3te3cXFBIpZtW0Les_PhgGK4ZkZs5ouAfTmmucyEdD53FN566qNJnkSl5Z42XK_VZAW2m-FCJCiljdIslrdST3vtEThECplfP7G8GQp_h-eCAKifOgwOrsG-gvWN4VdDMWKtchreJwGhNB6pPGz5ZPCAaCgYKAQwSARESFQG1tDrp-WG1K6SYl-JCjXH6xc6s6Q0166";
+ 
+const SHEET_ID = '1Se6P1VkG8IlgK2N6TzphvumLt13yNMOBjEITNfKToko';
+
+fetch(
+    `https://sheets.googleapis.com/v4/spreadsheets/1Se6P1VkG8IlgK2N6TzphvumLt13yNMOBjEITNfKToko/values/doctores`,
+    {
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${ACCESS_TOKEN}`,
+        },    
+    }
+).then(function (response) {
+    //esperamos el json del response para poder utilizarlo
+    response.json().then(function (data) {
+        const values = data.values;
+    
+        // Obtenemos el elemento del dom
+        const lista = document.getElementById("doctores");
+        const alerta = document.getElementById('alerta');
+        alerta.style.display = 'none';
+    
+    
+        for (var i = 1; i < values.length; i++) {
+
+            const doctor = document.createElement("div");
+            doctor.className =  "doctor-item";
+
+            // Nombre del doctor en lista
+            const doctorName = document.createElement("li");
+            doctorName.className = "nombre doc";
+            doctorName.innerHTML = 'Dr.' + values[i][0]; 
+    
+            // otros datos del doc tipo p
+            const fechaDisp = document.createElement("p");
+            fechaDisp.className = "fecha";
+            fechaDisp.innerHTML = 'Los días: ' + values[i][2];
+
+            const horario = document.createElement('p');
+            horario.className='horas';
+            horario.innerHTML = 'De ' + values[i][1];
+    
+            const especialidad = document.createElement('p');
+            especialidad.className='esp';
+            especialidad.innerHTML = 'Especialidad: ' + values[i][3];
+
+            // Agregamos todos los elementos al div de producto
+            doctor.appendChild(doctorName);
+            doctor.appendChild(fechaDisp);
+            doctor.appendChild(horario);
+            doctor.appendChild(especialidad);
+
+            // Agregamos el producto a la lista
+            lista.appendChild(doctor);
+        }
+    });
+});
+ 
