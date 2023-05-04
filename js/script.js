1d08c5970015df6edf55528b402a73d2c888bdf5
Annia Benítez <anniamicaela@gmail.com>
2023-05-03 21:51:19 -0400

    Inicié a hacer el codigo de script.js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,63 @@
-//CONSTANTES Y VARIABLES
-const SHEET_ID = "1Se6P1VkG8IlgK2N6TzphvumLt13yNMOBjEITNfKToko";
 const ACCESS_TOKEN =
-  "ya29.a0AWY7Cknx3scZNxJrXPT_TBsaIMMgre6BiomVv-V70xIOJ0eTorU4TAF-AMLc72gAY2nLEbHza3EucvSHLwwVHsHaVlgPf-sPN8akQt6JhY_7E0qrgCghH4BP8aGkych3z27FLc5nKiO4gI16Zqj4EF-2HMX0aCgYKAWMSARESFQG1tDrpK-_YNqpTsYyiiYjzVn9KTA0163";
+  "ya29.a0AWY7CklUub8Pv_NCas0_jee35X6PcB4HrQNN-Ih1czs1wRZx2h1K7qX-25F8HkdR14AfbxVFxnlB2GUocPJ3RSbc2aTndw6J7KSDQyNEAsr_MOyIe5pgR5VRatQmJm5R_WN_t9FzH1VxjdvISUm_K7gpL7t1HccaCgYKAUcSARESFQG1tDrpgOJNl9s0lKwFE56hYB1vtQ0166";
+ 
+const SHEET_ID = '1Se6P1VkG8IlgK2N6TzphvumLt13yNMOBjEITNfKToko';
 
-
-//Se obtiene la API del listado de pacientes que pidieron consulta
-fetch(`https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/enEspera`,
-  {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${ACCESS_TOKEN}`,
-    },
-  }
+fetch(
+    `https://sheets.googleapis.com/v4/spreadsheets/1Se6P1VkG8IlgK2N6TzphvumLt13yNMOBjEITNfKToko/values/enEspera`,
+    {
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${ACCESS_TOKEN}`,
+        },    
+    }
 ).then(function (response) {
     //esperamos el json del response para poder utilizarlo
     response.json().then(function (data) {
-    const values = data.values;
-    console.log(values);
+        const values = data.values;
     
+        // Obtenemos el elemento del dom
+        const lista = document.getElementById("formulario");
+        const alerta = document.getElementById('botones');
+        alerta.style.display = 'none';
+    
+      //Se ordenan los elementos por fecha y hora 
+        for(var i=0; i<values.length; i++){
+          
+        }
+
+        for (var i = 1; i < values.length; i++) {
+
+            const consulta = document.createElement("div");
+            consulta.className =  "consulta-item";
+
+            // Nombre del doctor en lista
+            const name = document.createElement("div");
+            name.className = "casilla";
+            doctorName.innerHTML = 'Paciente: NOMBRE APELLIDO - FECHA HORA'; 
+    
+            // otros datos del doc tipo p
+            const fechaDisp = document.createElement("p");
+            fechaDisp.className = "fecha";
+            fechaDisp.innerHTML = 'Los días: ' + values[i][2];
+
+            const horario = document.createElement('p');
+            horario.className='horas';
+            horario.innerHTML = 'De ' + values[i][1];
+    
+            const especialidad = document.createElement('p');
+            especialidad.className='esp';
+            especialidad.innerHTML = 'Especialidad: ' + values[i][3];
+
+            // Agregamos todos los elementos al div de producto
+            consulta.appendChild(doctorName);
+            consulta.appendChild(fechaDisp);
+            consulta.appendChild(horario);
+            consulta.appendChild(especialidad);
+
+            // Agregamos el producto a la lista
+            lista.appendChild(consulta);
+        }
     });
 });
+ 
