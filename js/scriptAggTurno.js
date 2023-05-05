5b2136e7192ff772a39b55a79bd337a3ebf1c48e
Annia Benítez <anniamicaela@gmail.com>
2023-05-05 11:17:22 -0400

    Se imprimen doctores en agg paci

--- js/scriptAggTurno.js
+++ js/scriptAggTurno.js
@@ -1,5 +1,5 @@
 const ACCESS_TOKEN =
-  "ya29.a0AWY7CknmjK-i6ULnzFtvbqm4NE3UHnsHMue9hC_tBKwYTymuIB-Bu_cGeK0K0YuiHmCLpEoNhnOiKcnHTurcBb-FzdrpVPSRu_UCtD2c4uF6kTIHFY6kq36w6Qv9U8RI4U6IhartSjsEAWWiVkDsZFQTIWOPaCgYKAc8SARESFQG1tDrpBWJsVQpqmkwJJoQHaCxipQ0163";
+  "ya29.a0AWY7CknC-O4-oxdsT2jlAyk2SDF535TGOLDjzYxmX9RBdw5IeIJjV2-AarEX-H3e9fVqeDiMvo7_v2L_gJkKurbcll7HgGKBFYzAhxosa9_Pw-RiP-xhV0hSBCO5EtZ6t2ykB308oiQ835ekbZ7s5vWn5zzbaCgYKASkSARESFQG1tDrpsIuuRixZSjDF7XAMutIXWA0163";
  
 const SHEET_ID = '1Se6P1VkG8IlgK2N6TzphvumLt13yNMOBjEITNfKToko';
 
@@ -49,3 +49,31 @@ function onRegistrarDato(){
 
     
 }
+
+fetch(
+    `https://sheets.googleapis.com/v4/spreadsheets/1Se6P1VkG8IlgK2N6TzphvumLt13yNMOBjEITNfKToko/values/doctores`,
+    {
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${ACCESS_TOKEN}`,
+        },    
+    }
+).then(function (response) {
+    //esperamos el json del response para poder utilizarlo
+    response.json().then(function (data) {
+        const values = data.values;
+    
+        // Obtenemos el elemento del dom
+        const lista = document.getElementById("doctor");  
+        //const lista2 = document.getElementById('horarios');  
+    
+        for (var i = 1; i < values.length; i++) {
+
+            const doctor = document.createElement("option");            
+            doctor.className =  "opcionDoc";
+            doctor.innerHTML = 'Dr. ' + values[i][0]; 
+            lista.appendChild(doctor);
+        }        
+    });
+});
+
