359401dfecbc1bddaf77ebb963bac5f4af232c3c
Annia Benítez <anniamicaela@gmail.com>
2023-05-05 12:11:15 -0400

    Apto para la carga de datos

--- js/scriptAggTurno.js
+++ js/scriptAggTurno.js
@@ -1,5 +1,5 @@
 const ACCESS_TOKEN =
-  "ya29.a0AWY7CknC-O4-oxdsT2jlAyk2SDF535TGOLDjzYxmX9RBdw5IeIJjV2-AarEX-H3e9fVqeDiMvo7_v2L_gJkKurbcll7HgGKBFYzAhxosa9_Pw-RiP-xhV0hSBCO5EtZ6t2ykB308oiQ835ekbZ7s5vWn5zzbaCgYKASkSARESFQG1tDrpsIuuRixZSjDF7XAMutIXWA0163";
+  "ya29.a0AWY7CkmYpC-KjYcnd6uWCNLeOelt7JgBPms1xUwspodUAgBlTHImg_Cs5DvSl-CgZbvMHHy_JX3zB-3givdT1VkOcQpHMG8BZDd0i7LtmthtZHLu2RbxPNBINX5WTWSyG5LFYPHxbX9OiHCRR88LB_8KKE8ssBAaCgYKASYSARESFQG1tDrpmYKJbl1ECONjBpXcI4sznw0166";
  
 const SHEET_ID = '1Se6P1VkG8IlgK2N6TzphvumLt13yNMOBjEITNfKToko';
 
@@ -8,12 +8,12 @@ document.getElementById('fecha').valueAsDate = new Date();
 function onRegistrarDato(){
 
     //Se obtienen los datos
-    const NOMBRE = document.getElementById('nombre').value;
-    const APELLIDO = document.getElementById('apellido').value;
-    const DOCTOR = document.getElementById('doctor').value;
-    const FECHA = document.getElementById('fecha').value;
-    const HORARIOS = document.getElementById('horarios').value;
-    const OBS = document.getElementById('obs').value;
+    let NOMBRE = document.getElementById('nombre').value;
+    let APELLIDO = document.getElementById('apellido').value;
+    let DOCTOR = document.getElementById('doctor').value;
+    let FECHA = document.getElementById('fecha').value;
+    //let HORARIOS = document.getElementById('horarios').value;
+    let OBS = document.getElementById('obs').value;
 
     if(NOMBRE == null || APELLIDO == null || DOCTOR == 'Seleccionar' || FECHA == 'Seleccionar' || OBS == null){
         alert('Debe completar todos los datos!!');
@@ -22,7 +22,7 @@ function onRegistrarDato(){
     else{
         let data = {};
         let values = [];
-        let fila = [NOMBRE, APELLIDO, FECHA, HORARIOS, DOCTOR, OBS, false];
+        let fila = [NOMBRE, APELLIDO, FECHA, /*HORARIOS,*/ DOCTOR, OBS, false];
         
         values.push(fila);
         data.range = "enEspera";
@@ -41,12 +41,11 @@ function onRegistrarDato(){
             }
         ).then(function (response) {
             response.json().then(function (data) {
-        
+                window.location.reload();
             });
         });
-        window.location.reload();
+        
     }
-
     
 }
 
